70c9b4dcd6a00983263e29f2578a8ea1b13a1b4c
xunilrj <xunilrj@hotmail.com>
2024-01-08 18:08:06 +0000

    contract entry using encoding

--- sway-core/src/semantic_analysis/program.rs
+++ sway-core/src/semantic_analysis/program.rs
@@ -1,19 +1,26 @@
 use crate::{
     language::{
-        parsed::{ParseProgram, Expression, ExpressionKind, MatchExpression, MatchBranch, CodeBlock, Scrutinee, AsmExpression, AsmRegisterDeclaration, FunctionApplicationExpression, AstNode, AstNodeContent, IfExpression, Declaration, VariableDeclaration, IntrinsicFunctionExpression, FunctionDeclaration},
-        ty::{self, TyProgram, TyFunctionDecl, TyDecl, TyAstNode}, Literal, AsmRegister, AsmOp, CallPath, Purity,
+        parsed::{
+            AsmExpression, AsmRegisterDeclaration, AstNode, AstNodeContent, CodeBlock, Declaration,
+            Expression, ExpressionKind, FunctionApplicationExpression, FunctionDeclaration,
+            IfExpression, IntrinsicFunctionExpression, MatchBranch, MatchExpression, ParseProgram,
+            Scrutinee, VariableDeclaration,
+        },
+        ty::{self, TyAstNode, TyDecl, TyFunctionDecl, TyProgram},
+        AsmOp, AsmRegister, CallPath, Literal, Purity,
     },
     metadata::MetadataManager,
     semantic_analysis::{
         namespace::{self, Namespace},
         TypeCheckContext,
     },
-    BuildConfig, Engines, TypeInfo, TypeBinding, TypeArgs, TypeArgument, transform::AttributesMap,
+    transform::AttributesMap,
+    BuildConfig, Engines, TypeArgs, TypeArgument, TypeBinding, TypeInfo,
 };
 use sway_ast::Intrinsic;
 use sway_error::handler::{ErrorEmitted, Handler};
 use sway_ir::{Context, Module};
-use sway_types::{Span, Ident, integer_bits::IntegerBits, Spanned};
+use sway_types::{integer_bits::IntegerBits, Ident, Span, Spanned};
 
 use super::{
     TypeCheckAnalysis, TypeCheckAnalysisContext, TypeCheckFinalization,
@@ -49,32 +56,18 @@ impl TyProgram {
         let modules_dep_graph = ty::TyModule::analyze(handler, root)?;
         let module_eval_order = modules_dep_graph.compute_order(handler)?;
 
-        dbg!(1);
-        let mut m = ty::TyModule::type_check(handler, ctx.by_ref(), root, module_eval_order).and_then(|root| {
-            let res = Self::validate_root(handler, engines, &root, kind.clone(), package_name);
-            res.map(|(kind, declarations, configurables)| Self {
-                kind,
-                root,
-                declarations,
-                configurables,
-                storage_slots: vec![],
-                logged_types: vec![],
-                messages_types: vec![],
-            })
-        })?;
+        let mut root = ty::TyModule::type_check(handler, ctx.by_ref(), root, module_eval_order)?;
 
-        dbg!(1);
-
-        if matches!(dbg!(&parsed.kind), crate::language::parsed::TreeType::Contract) {
+        if matches!(
+            dbg!(&parsed.kind),
+            crate::language::parsed::TreeType::Contract
+        ) {
             // /// Where 73 is the current offset in words from the start of the call frame.
             // const FIRST_PARAMETER_OFFSET: u64 = 73;
             // frame_ptr().add::<u64>(FIRST_PARAMETER_OFFSET).read()
-            fn call_decode_first_param(engines: &Engines,) -> Expression {
-                let string_slice_type_id = engines.te().insert(
-                    engines,
-                    TypeInfo::StringSlice,
-                    None,
-                );
+            fn call_decode_first_param(engines: &Engines) -> Expression {
+                let string_slice_type_id =
+                    engines.te().insert(engines, TypeInfo::StringSlice, None);
                 Expression {
                     kind: ExpressionKind::FunctionApplication(Box::new(
                         FunctionApplicationExpression {
@@ -84,30 +77,23 @@ impl TyProgram {
                                     suffix: Ident::new_no_span("decode_first_param".into()),
                                     is_absolute: false,
                                 },
-                                type_arguments: TypeArgs::Regular(vec![
-                                    TypeArgument {
-                                        type_id: string_slice_type_id,
-                                        initial_type_id: string_slice_type_id,
-                                        span: Span::dummy(),
-                                        call_path_tree: None,
-                                    },
-                                ]),
+                                type_arguments: TypeArgs::Regular(vec![TypeArgument {
+                                    type_id: string_slice_type_id,
+                                    initial_type_id: string_slice_type_id,
+                                    span: Span::dummy(),
+                                    call_path_tree: None,
+                                }]),
                                 span: Span::dummy(),
                             },
-                            arguments: vec![
-                            ],
+                            arguments: vec![],
                         },
                     )),
                     span: Span::dummy(),
                 }
             }
-            
+
             fn call_eq(engines: &Engines, l: Expression, r: Expression) -> Expression {
-                let string_slice_type_id = engines.te().insert(
-                    engines,
-                    TypeInfo::Boolean,
-                    None,
-                );
+                let string_slice_type_id = engines.te().insert(engines, TypeInfo::Boolean, None);
                 Expression {
                     kind: ExpressionKind::FunctionApplication(Box::new(
                         FunctionApplicationExpression {
@@ -127,87 +113,65 @@ impl TyProgram {
                 }
             }
 
-            let unit_type_id = engines.te().insert(
-                engines,
-                TypeInfo::Tuple(vec![]),
-                None,
-            );
-            let string_slice_type_id = engines.te().insert(
-                engines,
-                TypeInfo::StringSlice,
-                None,
-            );
+            let unit_type_id = engines.te().insert(engines, TypeInfo::Tuple(vec![]), None);
+            let string_slice_type_id = engines.te().insert(engines, TypeInfo::StringSlice, None);
 
-            let mut contents = vec![
-                AstNode { 
-                    content: AstNodeContent::Declaration(
-                        Declaration::VariableDeclaration(
-                            VariableDeclaration { 
-                                name: Ident::new_no_span("method_name".to_string()), 
-                                type_ascription: TypeArgument { 
-                                    type_id: string_slice_type_id,
-                                    initial_type_id: string_slice_type_id,
-                                    span: Span::dummy(),
-                                    call_path_tree: None
-                                }, 
-                                body: call_decode_first_param(engines), 
-                                is_mutable: false
-                            }
-                        )
-                    ),
-                    span: Span::dummy()
-                }
-            ];
+            let mut contents = vec![AstNode {
+                content: AstNodeContent::Declaration(Declaration::VariableDeclaration(
+                    VariableDeclaration {
+                        name: Ident::new_no_span("method_name".to_string()),
+                        type_ascription: TypeArgument {
+                            type_id: string_slice_type_id,
+                            initial_type_id: string_slice_type_id,
+                            span: Span::dummy(),
+                            call_path_tree: None,
+                        },
+                        body: call_decode_first_param(engines),
+                        is_mutable: false,
+                    },
+                )),
+                span: Span::dummy(),
+            }];
 
             let method_name_var_ref = Expression {
-                kind: ExpressionKind::Variable(
-                    Ident::new_no_span("method_name".to_string())
-                ),
+                kind: ExpressionKind::Variable(Ident::new_no_span("method_name".to_string())),
                 span: Span::dummy(),
             };
 
-            for (fn_decl, _) in  m.entry_fns(engines.de()) {
-                contents.push(AstNode { 
-                    content: AstNodeContent::Expression(
-                        Expression {
-                            kind: ExpressionKind::If(
-                                IfExpression {
-                                    // call eq
-                                    condition: Box::new(
-                                        call_eq(
-                                            engines,
-                                            method_name_var_ref.clone(),
-                                            Expression {
-                                                kind: ExpressionKind::Literal(
-                                                    Literal::String(fn_decl.name.span())
-                                                ),
-                                                span: Span::dummy(),
-                                            }
-                                        )
-                                    ),
-                                    then: Box::new(
-                                        Expression {
-                                            kind: ExpressionKind::IntrinsicFunction(
-                                                IntrinsicFunctionExpression {
-                                                    name: Ident::new_no_span("__log".to_string()),
-                                                    kind_binding: TypeBinding {
-                                                        inner: Intrinsic::Log,
-                                                        type_arguments: TypeArgs::Regular(vec![]),
-                                                        span: Span::dummy(),
-                                                    },
-                                                    arguments: vec![method_name_var_ref.clone()],
-                                                }
-                                            ),
+            for (fn_decl, _) in root.entry_fns(engines.de()) {
+                contents.push(AstNode {
+                    content: AstNodeContent::Expression(Expression {
+                        kind: ExpressionKind::If(IfExpression {
+                            // call eq
+                            condition: Box::new(call_eq(
+                                engines,
+                                method_name_var_ref.clone(),
+                                Expression {
+                                    kind: ExpressionKind::Literal(Literal::String(
+                                        fn_decl.name.span(),
+                                    )),
+                                    span: Span::dummy(),
+                                },
+                            )),
+                            then: Box::new(Expression {
+                                kind: ExpressionKind::IntrinsicFunction(
+                                    IntrinsicFunctionExpression {
+                                        name: Ident::new_no_span("__log".to_string()),
+                                        kind_binding: TypeBinding {
+                                            inner: Intrinsic::Log,
+                                            type_arguments: TypeArgs::Regular(vec![]),
                                             span: Span::dummy(),
-                                        }
-                                    ),
-                                    r#else: None,
-                                }
-                            ),
-                            span: Span::dummy(),
-                        }
-                    ), 
-                    span: Span::dummy()
+                                        },
+                                        arguments: vec![method_name_var_ref.clone()],
+                                    },
+                                ),
+                                span: Span::dummy(),
+                            }),
+                            r#else: None,
+                        }),
+                        span: Span::dummy(),
+                    }),
+                    span: Span::dummy(),
                 });
             }
 
@@ -233,18 +197,16 @@ impl TyProgram {
             };
 
             dbg!("__entry");
-            m.root.all_nodes.push(
-                TyAstNode::type_check (
-                    handler,
-                    ctx,
-                    AstNode {
-                        content: AstNodeContent::Declaration(
-                             Declaration::FunctionDeclaration(entry_fn_decl)
-                        ),
-                        span: Span::dummy(),
-                    }
-                )?
-            );
+            root.all_nodes.push(TyAstNode::type_check(
+                handler,
+                ctx,
+                AstNode {
+                    content: AstNodeContent::Declaration(Declaration::FunctionDeclaration(
+                        entry_fn_decl,
+                    )),
+                    span: Span::dummy(),
+                },
+            )?);
 
             // m.declarations.push(
             //     TyDecl::type_check(
@@ -255,9 +217,18 @@ impl TyProgram {
             // );
         }
 
-        dbg!(1);
+        let (kind, declarations, configurables) =
+            Self::validate_root(handler, engines, &root, kind.clone(), package_name)?;
 
-        Ok(m)
+        Ok(Self {
+            kind,
+            root,
+            declarations,
+            configurables,
+            storage_slots: vec![],
+            logged_types: vec![],
+            messages_types: vec![],
+        })
     }
 
     pub(crate) fn get_typed_program_with_initialized_storage_slots(
